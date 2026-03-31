733f835b57e4bdd08a5d7464885930e4d5abb020
Vaibhav Sarode <119931857+vaibhav-devops17@users.noreply.github.com>
2026-03-31 14:31:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
+app.use(express.json());
+
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/test";
+
+mongoose.connect(mongoURI)
+  .then(() => console.log("Mongo Connected"))
+  .catch(err => console.log(err));
+
+const Item = mongoose.model("Item", { name: String });
+
+app.get("/api", async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+app.post("/api", async (req, res) => {
+  const item = new Item({ name: req.body.name });
+  await item.save();
+  res.json(item);
+});
+
+app.listen(5000, () => console.log("Server running"));
